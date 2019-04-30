6d4f4e1be1df5d89dcad26764731848fde4ef52e
Topper Bowers <topper@toppingdesign.com>
2019-04-30 11:51:14 +0200

    add a simulated UI and use that for the game test

--- game/game_test.go
+++ game/game_test.go
@@ -1,3 +1,32 @@
 package game
 
-func
+import (
+	"testing"
+	"time"
+
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/quorumcontrol/jasons-game/navigator"
+	"github.com/quorumcontrol/jasons-game/ui"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNavigation(t *testing.T) {
+	rootCtx := actor.EmptyRootContext
+	simulatedUI, err := rootCtx.SpawnNamed(ui.NewSimulatedUIProps(), "test-navigation-ui")
+	require.Nil(t, err)
+	defer simulatedUI.Stop()
+
+	game, err := rootCtx.SpawnNamed(NewGameProps(simulatedUI, DefaultTree), "test-navigation-game")
+	require.Nil(t, err)
+	defer game.Stop()
+
+	rootCtx.Send(game, &ui.UserInput{Message: "north"})
+	time.Sleep(10 * time.Millisecond)
+	fut := rootCtx.RequestFuture(simulatedUI, &ui.GetEventsFromSimulator{}, 1*time.Second)
+	evts, err := fut.Result()
+
+	assert.Len(t, evts.([]interface{}), 2)
+	assert.IsType(t, &navigator.Location{}, evts.([]interface{})[0])
+	assert.IsType(t, &navigator.Location{}, evts.([]interface{})[1])
+}
