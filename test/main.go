3ac55d57418518a91cb2a87d99214b006e45ef30
Topper Bowers <topper@toppingdesign.com>
2019-05-02 15:00:49 +0200

    infura post

--- test/main.go
+++ test/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
 
 	"github.com/ipfs/go-datastore"
 	logging "github.com/ipfs/go-log"
+	"github.com/pkg/errors"
 	"github.com/quorumcontrol/chaintree/safewrap"
 	"github.com/quorumcontrol/jasons-game/network"
 )
@@ -23,6 +28,16 @@ func main() {
 	panicErr(sw.Err)
 	fmt.Printf("putting %s\n", n.Cid().String())
 	cli.Add(ctx, n)
+
+	reader := bytes.NewReader(n.RawData())
+
+	resp, err := newfileUploadRequest(
+		"https://ipfs.infura.io:5001/api/v0/dag/put?format=cbor&input-enc=raw",
+		nil, "file", reader)
+	panicErr(err)
+
+	log.Infof("resp %v", resp)
+
 	<-make(chan struct{})
 }
 
@@ -30,4 +45,34 @@ func panicErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+// Creates a new file upload http request with optional extra params
+func newfileUploadRequest(uri string, params map[string]string, paramName string, file io.Reader) (*http.Response, error) {
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile(paramName, "memory.cbor")
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(part, file)
+
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", uri, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	if err != nil {
+		return nil, errors.Wrap(err, "error making request")
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	return resp, err
 }
