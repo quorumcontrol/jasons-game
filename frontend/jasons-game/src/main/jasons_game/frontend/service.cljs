e0d4ce2fce1ae4d84c86fcaa2f20c79cd998d2f5
Topper Bowers <topper@toppingdesign.com>
2019-05-06 11:35:22 +0200

    initial setup with books

--- frontend/jasons-game/src/main/jasons_game/frontend/service.cljs
+++ frontend/jasons-game/src/main/jasons_game/frontend/service.cljs
@@ -1,5 +1,5 @@
 (ns jasons-game.frontend.service
   (:require ["/frontend/books_pb" :as book-lib :refer (Book)]))
 
-(let [my-book (new Book)]
+(let [my-book (new Book (cljs->js {:isbn 1234}))]
   (.log js/console "book: " (.getIsbn my-book)))
