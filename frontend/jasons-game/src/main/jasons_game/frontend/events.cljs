555c40fd0e71b9e4f00f002eb49607f08f4c508b
Topper Bowers <topper@toppingdesign.com>
2019-05-06 21:18:14 +0200

    some basic plumbing for sending off messages

--- frontend/jasons-game/src/main/jasons_game/frontend/events.cljs
+++ frontend/jasons-game/src/main/jasons_game/frontend/events.cljs
@@ -2,11 +2,27 @@
   (:require [re-frame.core :as re-frame]
             [day8.re-frame.tracing :refer-macros [fn-traced]]))
 
+
+(defn game-messages-query
+  [db v]         ;; db is current app state, v the query vector
+  (:game/messages db))
+
+(re-frame.core/reg-sub  ;; part of the re-frame API
+ :game-messages         ;; query id  
+ game-messages-query)            ;; query fn
+
 (re-frame/reg-event-db
  :initialize-db
  (fn-traced  [_ _]
-            ;  (service/get-book "http://localhost:8080" "1234" (fn [res] (.log js/console res)))
-             {:page :home}))
+             {:game/messages []
+              :nav/page :home}))
+
+(defn handle-user-input [{:keys [db]} [_ item]]
+  {:db (update db :game/messages #(conj % item))})
+
+(re-frame.core/reg-event-fx   ;; a part of the re-frame API
+ :user-input                ;; the kind of event
+ handle-user-input)
 
 ; (re-frame/reg-event-db
 ;  :routes/home
--- frontend/jasons-game/src/main/jasons_game/frontend/views.cljs
+++ frontend/jasons-game/src/main/jasons_game/frontend/views.cljs
@@ -1,10 +1,22 @@
 (ns jasons-game.frontend.views
   (:require
+   [reagent.core :as r]
+   [re-frame.core :as re-frame :refer [subscribe dispatch]]
    [jasons-game.frontend.components.terminal :as terminal]
    ["react"]
    ["react-dom" :as ReactDOM]
-   ["semantic-ui-react" :refer (Container)]))
+   ["semantic-ui-react" :refer [Container Input Button]]))
+
+(defn user-message [msg]
+  [:p msg])
 
 (defn app-root []
   [:> Container {:text true}
-   [:p "hi!"]])
+   [:> Container {:text true}
+    (let [messages (subscribe [:game-messages])]
+      (map user-message @messages))]
+    ;; TODO: put the text here]
+   (let [input-state (r/atom "")]
+     [:div
+       [:> Input {:onChange (fn [evt] (reset! input-state (-> evt .-target .-value))) :placeholder "What do you want to do?"}]
+       [:> Button {:onClick #(dispatch [:user-input @input-state])} "Send"]])])
