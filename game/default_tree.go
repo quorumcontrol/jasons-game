fa79f2e0e9f54ff4feddb11b954b0061c1dfead4
Brandon Westcott <brandon@quorumcontrol.com>
2019-07-08 13:52:34 -0400

    Hogwarts story

--- game/default_tree.go
+++ game/default_tree.go
@@ -15,36 +15,77 @@ func createHome(n network.Network) (*LocationTree, error) {
 
 	homeLocation := NewLocationTree(n, homeTree)
 
-	err = homeLocation.SetDescription("hi, welcome")
+	err = homeLocation.SetDescription("You are in the main hall of Hogwarts. As you observe your surroundings, you see a particularly interesting gargoyle statue on the wall")
 	if err != nil {
 		return nil, errors.Wrap(err, "error updating home tree")
 	}
 
-	northTree, err := n.CreateChainTree()
+	healingTree, err := n.CreateChainTree()
 	if err != nil {
 		return nil, errors.Wrap(err, "error creating tree")
 	}
-
-	northLocation := NewLocationTree(n, northTree)
-	err = northLocation.SetDescription("north of welcome")
+	healingLocation := NewLocationTree(n, healingTree)
+	err = healingLocation.SetDescription("You took a critical hit and were rushed to the temple of healing. Maybe next time you should fight the dragon with courage.")
 	if err != nil {
-		return nil, errors.Wrap(err, "error updating north tree")
+		return nil, errors.Wrap(err, "error updating healing tree")
 	}
-
-	err = northLocation.AddInteraction(&ChangeLocationInteraction{
-		Command: "south",
-		Did:     homeTree.MustId(),
+	err = healingLocation.AddInteraction(&ChangeLocationInteraction{
+		Command: "leave the temple",
+		Did:     homeLocation.MustId(),
 	})
 	if err != nil {
-		return nil, errors.Wrap(err, "error adding interaction to north tree")
+		return nil, errors.Wrap(err, "error adding interaction to cipher tree")
 	}
 
-	err = homeLocation.AddInteraction(&ChangeLocationInteraction{
-		Command: "north",
-		Did:     northLocation.MustId(),
+	hiddenTree, err := n.CreateChainTree()
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating tree")
+	}
+	hiddenLocation := NewLocationTree(n, hiddenTree)
+	err = hiddenLocation.SetDescription("The gargoyle slides aside and you enter the headmaster's chambers. Upon entering, you see a giant dragon staring you down.")
+	if err != nil {
+		return nil, errors.Wrap(err, "error updating hidden tree")
+	}
+	err = hiddenLocation.AddInteraction(&ChangeLocationInteraction{
+		Command: "run away",
+		Did:     homeLocation.MustId(),
 	})
 	if err != nil {
-		return nil, errors.Wrap(err, "error adding interaction to home tree")
+		return nil, errors.Wrap(err, "error adding interaction to cipher tree")
+	}
+
+	cmd2 := "fight the dragon"
+	secret2 := "with courage"
+	successInteraction2 := &RespondInteraction{
+		Response: "you killed the dragon, now what?",
+	}
+	failureInteraction2 := &ChangeLocationInteraction{
+		Did: healingLocation.MustId(),
+	}
+	ci2, err := NewCipherInteraction(cmd2, secret2, successInteraction2, failureInteraction2)
+	if err != nil {
+		return nil, errors.Wrap(err, "cipher tree error")
+	}
+	err = hiddenLocation.AddInteraction(ci2)
+	if err != nil {
+		return nil, errors.Wrap(err, "cipher tree error")
+	}
+
+	cmd := "whisper to the gargoyle"
+	secret := "sherbert lemon"
+	failureInteraction := &RespondInteraction{
+		Response: "Nothing happens. You remember professor McGonagall uttering a specific phrase to enter Dumbledore's chambers, maybe you should try that.",
+	}
+	successInteraction := &ChangeLocationInteraction{
+		Did: hiddenLocation.MustId(),
+	}
+	ci, err := NewCipherInteraction(cmd, secret, successInteraction, failureInteraction)
+	if err != nil {
+		return nil, errors.Wrap(err, "cipher tree error")
+	}
+	err = homeLocation.AddInteraction(ci)
+	if err != nil {
+		return nil, errors.Wrap(err, "cipher tree error")
 	}
 
 	return homeLocation, nil
