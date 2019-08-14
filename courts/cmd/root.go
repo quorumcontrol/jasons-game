2be4f5cad27be63d39bd8c86830ffc6d90f5aa40
Brandon Westcott <brandon@quorumcontrol.com>
2019-08-14 12:16:42 -0400

    Add cmd for courts

---
+++ courts/cmd/root.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	logging "github.com/ipfs/go-log"
+	"github.com/spf13/cobra"
+)
+
+var localNetworkFlag bool
+var configDir string
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "courts",
+	Short: "courts service for game interactions in each court",
+	Run: func(cmd *cobra.Command, args []string) {
+		err := cmd.Help()
+		if err != nil {
+			panic(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVar(&localNetworkFlag, "local", false, "should this use local tupelo/jason, defaults to false")
+	rootCmd.PersistentFlags().StringVar(&configDir, "config", "", "path to yaml config directory")
+	if err := rootCmd.MarkPersistentFlagRequired("config"); err != nil {
+		panic(err)
+	}
+
+	mustSetLogLevel("importer", "info")
+	mustSetLogLevel("respawner", "info")
+}
+
+func mustSetLogLevel(name string, level string) {
+	if err := logging.SetLogLevel(name, level); err != nil {
+		panic(err)
+	}
+}
