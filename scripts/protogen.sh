e90800378e6d0d8692937a6fbc805c9d16496b49
Wes Morgan <wesmorgan@icloud.com>
2019-08-26 13:59:18 -0600

    protoc-gen-ts moved to top level node_modules 🤷🏼‍♂️

--- scripts/protogen.sh
+++ scripts/protogen.sh
@@ -9,7 +9,7 @@ mkdir -p ./pb/jasonsgame
 mkdir -p ./frontend/jasons-game/src/js/frontend/remote
 
 protoc -I=./pb -I=$GOPATH/src -I=${GOPATH}/src/github.com/gogo/protobuf/protobuf jasonsgame.proto \
---plugin=protoc-gen-ts=./frontend/jasons-game/node_modules/.bin/protoc-gen-ts \
+--plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
 --ts_out=service=true:./frontend/jasons-game/src/js/frontend/remote \
 --js_out=import_style=commonjs,binary:./frontend/jasons-game/src/js/frontend/remote \
 --gogofaster_out=Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,plugins=grpc:./pb/jasonsgame
