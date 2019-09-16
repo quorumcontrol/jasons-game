900bf258a19843840899bb37fafffd516c1e9064
ben lamothe <bl@732.one>
2019-09-16 08:35:32 -0400

    consolidate internal and external receive ink files

--- network/receive_ink.go
+++ network/receive_ink.go
@@ -1,24 +1,13 @@
-// +build !internal
-
 package network
 
 import (
+	"context"
 	"crypto/ecdsa"
 
 	"github.com/pkg/errors"
-
 	"github.com/quorumcontrol/messages/build/go/transactions"
 	"github.com/quorumcontrol/tupelo-go-sdk/consensus"
 )
-
-func (n *RemoteNetwork) ReceiveInk(tokenPayload *transactions.TokenPayload) error {
-	inkWell, err := n.inkWell()
-	if err != nil {
-		return errors.Wrap(err, "error fetching inkwell")
-	}
-
-	return n.receiveInk(inkWell, n.signingKey, tokenPayload)
-}
 
 func (n *RemoteNetwork) DepositInk(source *consensus.SignedChainTree, key *ecdsa.PrivateKey, amount uint64) error {
 	well, err := n.inkWell()
@@ -39,6 +28,33 @@ func (n *RemoteNetwork) DepositInk(source *consensus.SignedChainTree, key *ecdsa
 	return n.receiveInk(well, n.signingKey, tokenPayload)
 }
 
+func (n *RemoteNetwork) ReceiveInk(tokenPayload *transactions.TokenPayload) error {
+	inkWell, err := n.inkWell()
+	if err != nil {
+		return errors.Wrap(err, "error fetching inkwell")
+	}
+
+	return n.receiveInk(inkWell, n.PrivateKey(), tokenPayload)
+}
+
 func (n *RemoteNetwork) ReceiveInkOnEphemeralChainTree(tree *consensus.SignedChainTree, privateKey *ecdsa.PrivateKey, tokenPayload *transactions.TokenPayload) error {
 	return n.receiveInk(tree, privateKey, tokenPayload)
+}
+
+func (n *RemoteNetwork) receiveInk(tree *consensus.SignedChainTree, privateKey *ecdsa.PrivateKey, tokenPayload *transactions.TokenPayload) error {
+	err := n.Tupelo.ReceiveInk(tree, privateKey, tokenPayload)
+	if err != nil {
+		return errors.Wrap(err, "error receiving ink")
+	}
+
+	err = n.TreeStore().SaveTreeMetadata(tree)
+	if err != nil {
+		return errors.Wrap(err, "error saving inkwell metadata after ink receive transaction")
+	}
+
+	log.Debug("receive ink saved inkwell metadata")
+
+	log.Debugf("inkwell chaintree after receive:\n%s", tree.ChainTree.Dag.Dump(context.TODO()))
+
+	return nil
 }
