1891bbd07c72d27b4f890296b24a709fe1070bce
Brandon Westcott <brandon@quorumcontrol.com>
2019-10-08 11:19:22 -0400

    Support concurrency for ServiceActor sending to handler

--- service/service_actor.go
+++ service/service_actor.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/AsynkronIT/protoactor-go/actor"
 	"github.com/AsynkronIT/protoactor-go/plugin"
+	"github.com/AsynkronIT/protoactor-go/router"
 	"github.com/gogo/protobuf/proto"
 	"github.com/pkg/errors"
 	"github.com/quorumcontrol/jasons-game/handlers"
@@ -16,39 +17,51 @@ import (
 type ServiceActor struct {
 	middleware.LogAwareHolder
 
-	tree       *consensus.SignedChainTree
-	network    network.Network
-	subscriber *actor.PID
-	handler    handlers.Handler
+	tree               *consensus.SignedChainTree
+	network            network.Network
+	subscriber         *actor.PID
+	handler            handlers.Handler
+	handlerPool        *actor.PID
+	handlerConcurrency int
+}
+
+type ServiceActorConfig struct {
+	Network     network.Network
+	Handler     handlers.Handler
+	Tree        *consensus.SignedChainTree
+	Concurrency int
 }
 
 type GetServiceDid struct{}
 
 func NewServiceActorProps(network network.Network, handler handlers.Handler) *actor.Props {
-	return actor.PropsFromProducer(func() actor.Actor {
-		return &ServiceActor{
-			network: network,
-			handler: handler,
-		}
-	}).WithReceiverMiddleware(
-		middleware.LoggingMiddleware,
-		plugin.Use(&middleware.LogPlugin{}),
-	)
+	return NewServiceActorPropsWithConfig(&ServiceActorConfig{
+		Network: network,
+		Handler: handler,
+	})
 }
 
 func NewServiceActorPropsWithTree(network network.Network, handler handlers.Handler, tree *consensus.SignedChainTree) *actor.Props {
+	return NewServiceActorPropsWithConfig(&ServiceActorConfig{
+		Network: network,
+		Handler: handler,
+		Tree:    tree,
+	})
+}
+
+func NewServiceActorPropsWithConfig(cfg *ServiceActorConfig) *actor.Props {
 	return actor.PropsFromProducer(func() actor.Actor {
 		return &ServiceActor{
-			network: network,
-			handler: handler,
-			tree: tree,
+			network:            cfg.Network,
+			handler:            cfg.Handler,
+			tree:               cfg.Tree,
+			handlerConcurrency: cfg.Concurrency,
 		}
 	}).WithReceiverMiddleware(
 		middleware.LoggingMiddleware,
 		plugin.Use(&middleware.LogPlugin{}),
 	)
 }
-
 
 func (s *ServiceActor) Receive(actorCtx actor.Context) {
 	switch msg := actorCtx.Message().(type) {
@@ -57,11 +70,8 @@ func (s *ServiceActor) Receive(actorCtx actor.Context) {
 	case *GetServiceDid:
 		actorCtx.Respond(s.tree.MustId())
 	case proto.Message:
-		s.Log.Infof("Received message %v", msg)
-		err := s.handler.Handle(msg)
-		if err != nil {
-			s.Log.Errorf(fmt.Sprintf("error handling %v: %v", proto.MessageName(msg), err))
-		}
+		s.Log.Debugf("Received message %v", msg)
+		actorCtx.Forward(s.handlerPool)
 	default:
 		s.Log.Errorf("Unhandled message %v", msg)
 	}
@@ -69,6 +79,10 @@ func (s *ServiceActor) Receive(actorCtx actor.Context) {
 
 func (s *ServiceActor) initialize(actorCtx actor.Context) {
 	var err error
+
+	if s.handlerConcurrency <= 0 {
+		s.handlerConcurrency = 1
+	}
 
 	if s.tree == nil {
 		did := consensus.EcdsaPubkeyToDid(*s.network.PublicKey())
@@ -91,5 +105,16 @@ func (s *ServiceActor) initialize(actorCtx actor.Context) {
 	}
 
 	topic := s.network.Community().TopicFor(s.tree.MustId())
+
+	s.handlerPool = actorCtx.Spawn(router.NewRoundRobinPool(s.handlerConcurrency).WithFunc(func(poolCtx actor.Context) {
+		switch msg := poolCtx.Message().(type) {
+		case proto.Message:
+			err := s.handler.Handle(msg)
+			if err != nil {
+				s.Log.Error(err)
+			}
+		}
+	}))
+
 	s.subscriber = actorCtx.Spawn(s.network.Community().NewSubscriberProps(topic))
 }
